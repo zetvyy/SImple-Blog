6d6fa8e3b4b6119c5051ca097cf3118720ab6cef
zetvyy <zetvyy@gmail.com>
2021-11-09 16:47:24 +0700

    fix redux

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,27 +1,68 @@
-import React from "react";
+import { Component } from "react";
+import { connect } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 
-const AddPost = () => {
-  return (
-    <div className="container">
-      <h5 className="blue-text center-align">Zaidan Blog</h5>
-      <form>
-        <div className="input-field">
-          <label htmlFor="post_title">Title</label>
-          <input type="text" />
-        </div>
-        <div className="input-field">
-          <label htmlFor="post_post">Content</label>
-          <textarea name="text-area" className="materialize-textarea"></textarea>
-        </div>
-        <div className="input-field">
-          <button className="btn blue">
-            Submit
-            <i className="material-icons right">send</i>
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+class AddPost extends Component {
+  state = {
+    post: {
+      id: "",
+      title: "",
+      content: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      post: {
+        ...this.state.post,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.createPost(this.state.post);
+    this.setState({
+      post: {
+        id: uuidv4(),
+        title: "",
+        content: ""
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h5 className="blue-text center-align">Zaidan Blog</h5>
+        <form>
+          <div className="input-field">
+            <label htmlFor="post_title">Title</label>
+            <input onChange={this.handleChange} value={this.state.post.title} type="text" name="title" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="post_post">Content</label>
+            <textarea onChange={this.handleChange} value={this.state.post.content} name="content" className="materialize-textarea"></textarea>
+          </div>
+          <div className="input-field">
+            <button onClick={this.handleSubmit} className="btn blue">
+              Submit
+              <i className="material-icons right">send</i>
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createPost: post => {
+      dispatch({ type: "ADD_POST", post });
+    }
+  };
 };
 
-export default AddPost;
+export default connect(null, mapDispatchToProps)(AddPost);
--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -1,17 +1,18 @@
 const initialState = {
-  posts: [
-    { id: 1, title: "post one", content: "this is post one" },
-    { id: 2, title: "post two", content: "this is post two" }
-  ]
+  posts: []
 };
 
 const postReducer = (state = initialState, action) => {
-  const { type, payLoad } = action;
+  const { type } = action;
   switch (type) {
     case "DELETE_POST":
       const newPost = state.posts.filter(post => post.id !== action.id);
       return {
         posts: newPost
+      };
+    case "ADD_POST":
+      return {
+        posts: [...state.posts, action.post]
       };
     default:
       return state;
